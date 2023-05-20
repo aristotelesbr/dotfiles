9bb6d97b95a08914902624a4329e1668cf0c9e66
aristotelesbr <aristotelesbr@gmail.com>
2023-05-20 18:42:11 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# My Dotfiles
+
+![screenshot](public/screenshot.png)
+
+## Instalation
+
+TODO: add description
+
+to install thi
+
+## Get healthy
+
+Open `nvim` and enter the following:
+
+```
+:checkhealth
+```
+
+You'll probably notice you don't have support for copy/paste also that python and node haven't been setup
+
+So let's fix that
+
+First we'll fix copy/paste
+
+- On mac `pbcopy` should be builtin
+
+- On Arch Linux
+
+  ```
+  sudo pacman -S xsel
+  ```
+  
+- Wayland users
+
+  [wl-clipboard](https://github.com/bugaevc/wl-clipboard)
+
+Next we need to install python support (node is optional)
+
+- Neovim python support
+
+  ```
+  pip install pynvim
+  ```
+
+- Neovim node support
+
+  ```
+  npm i -g neovim
+  ```
+---
+
+### Neovim version
+
+    ```
+    NVIM v0.9.0
+    Build type: Release
+    LuaJIT 2.1.0-beta3
+    ```
+
+Aristóteles Dotfiles
---
+++ lua/aristoteles/autocommands.lua
@@ -0,0 +1,83 @@
+-- _general_settings
+local general_settings_group = vim.api.nvim_create_augroup("_general_settings", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = general_settings_group,
+  pattern = "qf,help,man,lspinfo",
+  command = "nnoremap <silent> <buffer> q :close<CR>"
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = general_settings_group,
+  pattern = "*",
+  command = "silent! lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})"
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = general_settings_group,
+  pattern = "*",
+  command = ":set formatoptions-=cro"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = general_settings_group,
+  pattern = "qf",
+  command = "set nobuflisted"
+})
+
+-- _git
+local git_group = vim.api.nvim_create_augroup("_git", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = git_group,
+  pattern = "gitcommit",
+  command = "setlocal wrap"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = git_group,
+  pattern = "gitcommit",
+  command = "setlocal spell"
+})
+
+-- _markdown
+local markdown_group = vim.api.nvim_create_augroup("_markdown", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = markdown_group,
+  pattern = "markdown",
+  command = "setlocal wrap"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = markdown_group,
+  pattern = "markdown",
+  command = "setlocal spell"
+})
+
+-- _auto_resize
+local auto_resize_group = vim.api.nvim_create_augroup("_auto_resize", { clear = true })
+
+vim.api.nvim_create_autocmd("VimResized", {
+  group = auto_resize_group,
+  command = "tabdo wincmd ="
+})
+
+-- _alpha
+local alpha_group = vim.api.nvim_create_augroup("_alpha", { clear = true })
+
+vim.api.nvim_command("augroup " .. alpha_group)
+vim.api.nvim_command("autocmd!")
+vim.api.nvim_command("autocmd User AlphaReady lua vim.api.nvim_set_option('showtabline', 0)")
+vim.api.nvim_command("autocmd BufUnload <buffer> lua vim.api.nvim_set_option('showtabline', 2)")
+vim.api.nvim_command("augroup END")
+
+-- _copilot
+local copilot_group = vim.api.nvim_create_augroup("_copilot", { clear = true })
+--
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = copilot_group,
+  pattern = "*",
+  command = "Copilot suggestion",
+})
+-- })
