b66335e45fc8d0662b5da59c3d135f87ea56e053
aristotelesbr <aristotelesbr@gmail.com>
2023-06-11 11:29:51 -0300

    update copilot settings

--- lua/aristoteles/copilot.lua
+++ lua/aristoteles/copilot.lua
@@ -1,40 +1,43 @@
 local config_status_ok, nvim_copilot = pcall(require, "copilot")
 if not config_status_ok then
-  return
+	return
 end
 
-nvim_copilot('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = false,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    ["*"]    = true,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 16.x
-  server_opts_overrides = {},
+nvim_copilot("copilot").setup({
+	panel = {
+		enabled = true,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4,
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = false,
+		debounce = 75,
+		keymap = {
+			accept = "<M-l>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		markdown = true,
+		json = true,
+		javascript = true,
+		["*"] = true,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 16.x
+	server_opts_overrides = {},
 })
